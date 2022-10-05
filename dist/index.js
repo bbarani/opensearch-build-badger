c32e2d71af679e91ef3f93bd22489073aaaca6e5
Barani <bbarani@amazon.com>
2022-10-05 12:05:16 -0700

    Generating the build code

--- dist/index.js
+++ dist/index.js
@@ -24,7 +24,6 @@ function run() {
         const versions = versionParam.split(",").map(str => str.trim());
         const repositories = [
             "OpenSearch",
-            "OpenSearch-Dashboards",
             "alerting",
             "anomaly-detection",
             "asynchronous-search",
@@ -42,6 +41,17 @@ function run() {
             "performance-analyzer-rca",
             "security",
             "sql",
+            "OpenSearch-Dashboards",
+            "index-management-dashboards-plugin",
+            "dashboards-maps",
+            "security-dashboards-plugin",
+            "security-analytics-dashboards-plugin ",
+            "alerting-dashboards-plugin",
+            "opensearch-dashboards-functional-test ",
+            "anomaly-detection-dashboards-plugin ",
+            "dashboards-visualizations",
+            "ml-commons-dashboards",
+            "dashboards-notebooks "
         ];
         const untriagedPrefix = "![](https://img.shields.io/github/issues/opensearch-project";
         const makeUntriagedBadge = (repo) => `[${untriagedPrefix}/${repo}/untriaged)](https://github.com/opensearch-project/${repo}/issues?q=is%3Aissue+is%3Aopen+label%3Auntriaged)`;
@@ -51,8 +61,8 @@ function run() {
         const makeClosedIssueBadge = (repo, twoDigitVer) => `[${closedIssuePrefix}/${repo}/v${twoDigitVer}.0)](https://github.com/opensearch-project/${repo}/issues?q=is%3Aissue+is%3Aclosed+label%3Av${twoDigitVer}.0)`;
         let lines = [];
         lines.push(`## Release Readiness
-    Repo | Triage | ${versions.join(" | ")}
-    -----|-----|-----|-----|-------`);
+Repo | Triage | ${versions.join(" | ")}
+-----|-----|-----|-----|-------`);
         repositories.forEach(repo => {
             let line = `${repo}`;
             line = `${line} | ${makeUntriagedBadge(repo)}`;
